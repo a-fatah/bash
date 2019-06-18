8221526a548ab44d358003aca0f4ac21b9c08961
Abdul Fatah <abdulfatah66@gmail.com>
2019-06-18 11:27:08 +0500

    nested if..then..else

--- conditionals.sh
+++ conditionals.sh
@@ -6,5 +6,10 @@ if [ $count -eq 100 ]
 then
   echo "Count is 100"
 else
-  echo "Count is not 100"
+  if [ $count -gt 100 ]
+  then
+    echo "Count is greater than 100"
+  else
+    echo "Count is less than 100"
+  fi
 fi
