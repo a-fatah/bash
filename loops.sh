206187e806f46cdbfe95cebaab8451f866c33ccc
Abdul Fatah <abdulfatah66@gmail.com>
2019-06-20 09:46:11 +0500

    conditional inside loop

--- loops.sh
+++ loops.sh
@@ -3,5 +3,9 @@
 friends=( "Marcus the Rich" "JJ The Short" "Timid Thomas" "Michelangelo The Mobster" )
 
 for friend in "${friends[@]}"; do
-    echo "$friend is my friend"
+    if [[ $friend = "${friends[0]}" ]]; then
+        echo "$friend was my first friend"
+    else
+        echo "$friend is my friend"
+    fi
 done
